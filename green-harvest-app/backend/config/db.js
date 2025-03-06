9715defb109b3e1b565a25068895ffd044ccc49d
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-03-07 00:01:33 +0800

    Added configurations for Azure SQL connection

--- green-harvest-app/backend/config/db.js
+++ green-harvest-app/backend/config/db.js
@@ -1,18 +1,28 @@
 const sql = require("mssql");
 const dotenv = require("dotenv");
+const { Sequelize } = require("sequelize");
 
 dotenv.config();
 
-const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  server: process.env.DB_HOST, // e.g. localhost
-  database: process.env.DB_NAME,
-  options: {
-    encrypt: false, // Set to true if using Azure
-    enableArithAbort: true,
+const sequelize = new Sequelize("GreenHarvest", "greenHarvestAdmin", "Greenharvest2025!", {
+  host: "greenharvestserver.database.windows.net",
+  dialect: "mssql",
+  dialectOptions: {
+    encrypt: true, // Required for Azure
   },
-};
+});
+
+//localDB
+// const config = {
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   server: process.env.DB_HOST, // e.g. localhost
+//   database: process.env.DB_NAME,
+//   options: {
+//     encrypt: false, // Set to true if using Azure
+//     enableArithAbort: true,
+//   },
+// };
 
 const connectDB = async () => {
   try {
