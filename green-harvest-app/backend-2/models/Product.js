7e057ee30f0c29c07ada1d622ba76e3f8ba3dba4
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-03-07 00:06:24 +0800

    Updated product model structure

--- green-harvest-app/backend-2/models/Product.js
+++ green-harvest-app/backend-2/models/Product.js
@@ -1,19 +1,40 @@
-const { DataTypes } = require("sequelize");
-const { sequelize } = require("../config/db");
+module.exports = (sequelize, DataTypes) => {
+    const Product = sequelize.define("Product", {
+      ProductID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        field: "ProductID"
+      },
+      Name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "Name"
+      },
+      Description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "Description"
+      },
+      Price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        field: "Price"
+      },
+      Stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: "Stock"
+      },
+      FarmerID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: "FarmerID"
+      }
+    }, {
+        tableName: "Products",
+        timestamps: false // Set to true if using createdAt/updatedAt
+    });
 
-// Define the Product model
-// The Product model represents the products table in the database
-const Product = sequelize.define("Products", {
-    ProductId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    Name: { type: DataTypes.STRING, allowNull: false },
-    Description: { type: DataTypes.STRING, allowNull: false },
-    Price: { type: DataTypes.FLOAT, allowNull: false },
-    Stock: { type: DataTypes.INTEGER, defaultValue: 0 },
-    FarmerId: { type: DataTypes.INTEGER, allowNull: false },
-}, {
-    tableName: "Products",
-    schema: "dbo",
-    timestamps: false //Add this to remove the createdAt and updatedAt fields
-  });
-
-module.exports = Product;
+    return Product;
+};
