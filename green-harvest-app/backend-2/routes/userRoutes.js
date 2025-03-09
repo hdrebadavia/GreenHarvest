d43ce9172d138259fbd66f48861ba40d9ffe4fc7
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-03-09 15:39:45 +0800

    Added resetPassword and getUserById methods in routes

--- green-harvest-app/backend-2/routes/userRoutes.js
+++ green-harvest-app/backend-2/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { registerUser, loginUser, getUsers } = require("../controllers/userController");
+const { registerUser, loginUser, getUsers, resetPassword, getUserById } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/", getUsers); // Fetch all users
-
+router.get("/resetPassword", resetPassword);
+router.get("/:id", getUserById)
 module.exports = router;
 
