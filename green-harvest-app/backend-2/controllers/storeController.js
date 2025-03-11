15a358403cabcada2dd1f6181df0f59e062e2ff9
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-03-11 17:56:51 +0800

    Update storeController.js

--- green-harvest-app/backend-2/controllers/storeController.js
+++ green-harvest-app/backend-2/controllers/storeController.js
@@ -1,6 +1,6 @@
 const db = require("../models");
-const { get } = require("../routes/productRoutes");
 const Store = db.Store;
+const moment = require('moment');
 
 // Get all stores
 const getStores = async (req, res) => {
@@ -33,16 +33,28 @@ const getStoreById = async (req, res) => {
     }
 }
 
-// Create a new store
+// #region Create a new store
 const createStore = async (req, res) => {
     try {
         const { Name, Description, Location, CreatedBy } = req.body;
-        const newStore = await Store.create({ Name, Description,  Location, CreatedBy });
-        res.status(201).json(newStore);
+        const newStore = await
+            Store.create({ Name,
+                            Description,
+                            Location,
+                            CreatedBy
+                        });
+
+        res.status(200).json(newStore);
     } catch (error) {
-        res.status(500).json({ message: "Error creating store", error });
+        if (error.name === 'SequelizeValidationError') {
+            const messages = error.errors.map(err => err.message);
+            return res.status(400).json({ message: "Validation Error", errors: messages });
+        }
+        res.status(500).json({ message: "Error creating store", error: error.message });
     }
 };
+
+// #region Update store
 
 const updateStore = async (req, res) => {
     try {
@@ -54,14 +66,36 @@ const updateStore = async (req, res) => {
         store.Name = Name || store.Name;
         store.Description = Description || store.Description;
         store.Location = Location || store.Location;
-        store.UpdatedBy = UpdatedBy || store.UpdatedBy;
+        store.UpdatedBy = UpdatedBy || store.UpdatedBy;// Format the date using Moment.js
         await store.save();
 
         res.json(store);
     } catch (error) {
-        res.status(500).json({ message: "Error updating store", error });
+        if (error.name === 'SequelizeValidationError') {
+            const messages = error.errors.map(err => err.message);
+            return res.status(400).json({ message: "Validation Error", errors: messages });
+        }
+        res.status(500).json({ message: "Error updating store", error: error.message });
     }
 };
 
+//#endregion
 
-module.exports = { getStores, getStoreById, createStore, updateStore };
+// #region Delete store
+const deleteStore = async (req, res) => {
+    try {
+        const store = await Store.findByPk(req.params.id);
+        if (!store) return res.status(404).json({ message: "Store not found" });
+
+        await store.destroy();
+        res.json({ message: "Store deleted successfully" });
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError') {
+            const messages = error.errors.map(err => err.message);
+            return res.status(400).json({ message: "Validation Error", errors: messages });
+        }
+        res.status(500).json({ message: "Error updating store", error: error.message });
+    }
+}
+//#endregion
+module.exports = { getStores, getStoreById, createStore, updateStore, deleteStore };
