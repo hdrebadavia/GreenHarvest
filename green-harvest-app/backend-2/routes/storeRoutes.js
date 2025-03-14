8628f775d7fc18fe65b9e60ff1cf451b8ead24d3
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-03-14 17:59:22 +0800

    Update storeRoutes.js

--- green-harvest-app/backend-2/routes/storeRoutes.js
+++ green-harvest-app/backend-2/routes/storeRoutes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.get("/", getStores);
 router.get("/:id", getStoreById);
-router.post("/createStore", createStore);
-router.patch("/updateStore/:id", updateStore);
-router.delete("/deleteStore/:id", deleteStore);
+router.post("/", createStore);
+router.patch("/:id", updateStore);
+router.delete("/:id", deleteStore);
 
 module.exports = router;
