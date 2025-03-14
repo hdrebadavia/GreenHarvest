ac098d7aa2f2b8bba8e1ed8afab8e1b0c8d88c66
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-03-14 17:59:15 +0800

    Update products.js

--- green-harvest-app/backend-2/models/products.js
+++ green-harvest-app/backend-2/models/products.js
@@ -85,9 +85,17 @@ module.exports = (sequelize, DataTypes) => {
     });
 
     // Define relationships
-    Product.associate = (models) => {
-      Product.belongsTo(models.Users, { foreignKey: "CreatedBy", as: "Creator" });
-    };
+    // Product.associate = (models) => {
+    //   Product.belongsTo(models.Users, { foreignKey: "CreatedBy", as: "Creator" });
+    //   Product.belongsTo(models.Users, { foreignKey: "UpdatedBy", as: "Updater" });
+    //   Product.belongsTo(models.Stores, { foreignKey: "StoreId", as: "Store" });
+    //   Product.hasMany(models.OrderItems, { foreignKey: "ProductId", as: "OrderItems" });
+    //   Product.hasMany(models.Orders, { foreignKey: "ProductId", as: "Orders" });
+    //   Product.hasMany(models.Addresses, { foreignKey: "ProductId", as: "Addresses" });
+    //   Product.hasMany(models.Stores, { foreignKey: "ProductId", as: "Stores" });
+    //   Product.hasMany(models.Users, { foreignKey: "ProductId", as: "Users" });
+    //   Product.hasMany(models.Products, { foreignKey: "ProductId", as: "Products" });
+    // };
 
     return Product;
 };
