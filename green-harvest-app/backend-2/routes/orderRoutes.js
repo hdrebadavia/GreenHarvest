21fcd15d91685d6b80fd9aae21a4afeef9494367
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-03-14 17:59:20 +0800

    Update orderRoutes.js

--- green-harvest-app/backend-2/routes/orderRoutes.js
+++ green-harvest-app/backend-2/routes/orderRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { createOrder, getOrders } = require("../controllers/orderController");
+const { createOrder, getOrders, getOrderById, updateOrder, deleteOrder } = require("../controllers/orderController");
 
 const router = express.Router();
 
 router.post("/", createOrder);
 router.get("/", getOrders);
+router.get("/:id", getOrderById);
+router.put("/:id", updateOrder);
+router.delete("/:id", deleteOrder);
 
 module.exports = router;
