575469d3242dc3d35273d7ac094ee8df2f7d7383
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-03-17 22:37:39 +0800

    Create login.component.ts

---
+++ green-harvest-app/frontend-2/green-harvest-app/frontend-2/src/app/user-management/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private apiService: ApiService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.apiService.login(this.loginForm.value).subscribe(
+        response => {
+          // Handle successful login
+        },
+        error => {
+          // Handle login error
+        }
+      );
+    }
+  }
+}
