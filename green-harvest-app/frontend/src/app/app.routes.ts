a720007343aee9c75a2ea2b32445e33a26698677
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-04-09 16:39:26 +0800

    Cleaned repository

--- green-harvest-app/frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './authentication/login/login.component';
-import { RegistrationComponent } from './authentication/registration/registration.component';
-
-export const appRoutes: Routes = [  // export appRoutes, not routes
-  { path: 'auth/login', component: LoginComponent },
-  { path: 'auth/register', component: RegistrationComponent },
-  { path: '**', redirectTo: '/auth/login', pathMatch: 'full' },
-];
