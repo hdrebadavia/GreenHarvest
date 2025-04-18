f7085456f9dd9510a19a925e1796af19d5981113
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-04-18 18:30:34 +0800

    Create user.interface.ts

---
+++ green-harvest-app/greenharvest-frontend/src/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User{
+    UserId: number;
+    FirstName: string;
+    MiddleName: string;
+    LastName: string;
+    EmailAddress: string;
+    ContactNumber: string;
+    Password: string;
+    Role: string;
+}
