142aa3ef94d5a2dc05c4e44df87769b5e719c46f
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-04-18 18:30:53 +0800

    Create user.service.ts

---
+++ green-harvest-app/greenharvest-frontend/src/services/user.service.ts
@@ -0,0 +1,28 @@
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../interfaces/user.interface';
+
+class UserService {
+  // Create a BehaviorSubject to store user details
+  private userDetailsSubject = new BehaviorSubject<User | null>(null);
+
+  // Observable stream for user details
+  userDetails$ = this.userDetailsSubject.asObservable();
+
+  // Method to update user details
+  setUserDetails(user: User) {
+    this.userDetailsSubject.next(user);
+  }
+
+  // Method to clear user details (e.g., on logout)
+  clearUserDetails() {
+    this.userDetailsSubject.next(null);
+  }
+
+  // Method to get the current user details (synchronously)
+  getCurrentUserDetails(): User | null {
+    return this.userDetailsSubject.getValue();
+  }
+}
+
+// Export an instance of the service
+export const userService = new UserService();
