9def64cea3ce21242cbcb4697ff28419a9996e5b
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-04-18 18:30:39 +0800

    Update api.ts

--- green-harvest-app/greenharvest-frontend/src/services/api.ts
+++ green-harvest-app/greenharvest-frontend/src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Product } from '../interfaces/product.interface';
 
 const api = axios.create({
   baseURL: 'http://localhost:5001/api', // adjust to match your ASP.NET backend
@@ -19,6 +20,14 @@ export const register = (
    return api.post(`/users/register`, data);
   };
 
+  export const getUser = (userId: number) => {
+    return api.get(`/users/${userId}`);
+  };
+
   export const getProducts = () => {
     return api.get('/products');
   }
+
+  export const addProduct = (data: Product) => {
+    return api.post('/products', data);
+  };
