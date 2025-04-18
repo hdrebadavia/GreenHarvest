119f5061cfc7b6f66d2cc35517eeea6961937393
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-04-18 18:30:24 +0800

    Create product.interface.ts

---
+++ green-harvest-app/greenharvest-frontend/src/interfaces/product.interface.ts
@@ -0,0 +1,13 @@
+export interface Product {
+    ProductId: number;
+    Name: string;
+    Description: string;
+    Price: number;
+    imageUrl: string;
+    ProductType: string;
+    Quantity: number;
+    Unit: string;
+    StoreId: number;
+    CreatedBy: number
+  }
+  
