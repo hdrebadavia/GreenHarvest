c3badd9382e01cf556b3b804e95aadac7980d8da
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-04-20 22:39:16 +0800

    Update App.tsx

--- green-harvest-app/greenharvest-frontend/src/App.tsx
+++ green-harvest-app/greenharvest-frontend/src/App.tsx
@@ -4,13 +4,18 @@ import Register from './components/auth/Register';
 import ProductPage from './components/products/products';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/products" element={<ProductPage />} />
+      <Route path="/products" element={
+        <ProtectedRoute>
+          <ProductPage />
+        </ProtectedRoute>
+      } />
     </Routes>
   );
 }
