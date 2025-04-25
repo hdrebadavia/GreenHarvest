89b6b59726ab80c324fd128ec0f728a72290c6ad
hdrebadavia <99799022+hdrebadavia@users.noreply.github.com>
2025-04-25 22:19:30 +0800

    Update stores.tsx

---
+++ green-harvest-app/greenharvest-frontend/src/components/admin/stores.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { Store } from '../../interfaces/store.interface';
+import { getStores } from '../../services/api';
+
+const Stores: React.FC = () => {
+    const [stores, setStores] = useState<Store[]>([])
+
+    const handleGetStores = async () => {
+        try{
+          const response = await getStores();
+          setStores(response.data);
+          console.log(stores)
+        }catch(err){
+          console.error('Error fetching stores:', err);
+        }
+      };
+      
+    useEffect(() => {
+        handleGetStores()
+    })
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <table className="table table-striped">
+                        <thead>
+                            <th>#</th>
+                            <th>Store Name</th>
+                            <th>Location</th>
+                        </thead>
+                        <tbody>
+                            {
+                                stores.map((store, index) => (
+                                    <tr key={store.StoreId || index}>
+                                        <td>{index}</td>
+                                        <td>{store.Name}</td>
+                                        <td>{store.Location}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Stores;
